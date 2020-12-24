b4f3e6120b4f858b2b9be1d5f948bb7d51c8438c
ha-mulan <54228425+ha-mulan@users.noreply.github.com>
2020-12-24 18:12:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 
 from flask import Flask
 from flask import request
-from primeFactor import PrimeFactor
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,11 +12,11 @@ def hello_world():
         if num==1 or num==0:
             return "{} has no primeFactor".format(num)
         elif 0<num and num<=1000000:
-            return 'biggest primeFactor of {} is {}'.format(num,PrimeFactor(num))
+            return 'biggest primeFactor of {} is {}'.format(num)
         else:
             return "input range error input must be range of 0~1000000"
     except:
         return 'NO INPUT!!! please give the input by adding  <  ?input={your input} > at the end of url'
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8000)))
