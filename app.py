fbaacaebaa1625a95cd9e3626879faaa78cd5afe
ha-mulan <54228425+ha-mulan@users.noreply.github.com>
2020-12-28 14:34:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 import os
-
+from subprocess import check_output
 from flask import Flask
 from flask import request
+from generator import *
+from ArticleGenerator import *
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     target = os.environ.get('TARGET', 'World')
-    try:
-        num=int(request.args.get('input'))
-        if num==1 or num==0:
-            return "{} has no primeFactor".format(num)
-        elif 0<num and num<=1000000:
-            return 'biggest primeFactor of {} is {}'.format(num)
-        else:
-            return "input range error input must be range of 0~1000000"
-    except:
-        return 'NO INPUT!!! please give the input by adding  <  ?input={your input} > at the end of url'
-
+    keyword=request.args.get('keyword')
+    list_news=Generator.generate(keyword,title=keyword)
+    listToStr = ' '.join(map(str,list_news))
+    return listToStr
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8000)))
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
+
+# host.docker.internal
