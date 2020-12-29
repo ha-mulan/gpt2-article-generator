292dd6adf442c41e6d55d5c282d6ecf5fcf1d072
ha-mulan <54228425+ha-mulan@users.noreply.github.com>
2020-12-29 11:03:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    #try:
-    target = os.environ.get('TARGET', 'World')
-    keyword=request.args.get('keyword')
-    list_news=Generator.generate(keyword,title=keyword)
-    listToStr = ' '.join(map(str,list_news))
-    return listToStr
-    #except:
-     #   return 'NO KEYWORD!!! please give the input by adding  <  ?keyword={your keyword} > at the end of url'
+    try:
+        target = os.environ.get('TARGET', 'World')
+        keyword=request.args.get('keyword')
+        list_news=Generator.generate(keyword,title=keyword)
+        listToStr = ' '.join(map(str,list_news))
+        return listToStr
+    except:
+       return 'NO KEYWORD!!! please give the input by adding  <  ?keyword={your keyword} > at the end of url'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',threaded=False,port=int(os.environ.get('PORT', 80)))
