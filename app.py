46f21621e7135c6394caddeb6463c69ad31685b1
ha-mulan <54228425+ha-mulan@users.noreply.github.com>
2020-12-30 16:35:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,5 @@
 from generator import *
 from ArticleGenerator import *
-# import os
-# from subprocess import check_output
-# from flask import Flask
-# from flask import request
-# app = Flask(__name__)
-
-# @app.route('/')
-# def homepage():
-#     # target = os.environ.get('TARGET', 'World')
-#     # try:
-#     # keyword=request.args.get('keyword')
-
-#     #stream = os.popen(f'python3 ArticleGenerator.py {keyword} -C {keyword} -p')
-#     # return check_output('ls -al')
-#     return 'to generate article please add /api/title/content at the end of url'
-
-
-# @app.route('/api/')
-# def api():
-#     titled=request.args.get('title')
-#     contented=request.args.get('content')
-
-#     Generatored=Generator.get_instance()
-#     # return titled+contented
-#     list_news=Generatored.generate(initial_content=contented,title=titled)
-#     listToStr = ' '.join(map(str,list_news))
-#     return listToStr
-#     # return title+content
-#         # return check_output(f'python3 ArticleGenerator.py {keyword} -C {keyword} -p')
-#     # except:
-#         # return 'NO KEYWORD!!! please give the input by adding  <  ?keyword={your keyword} > at the end of url'
-
-# if __name__ == "__main__":
-#     app.run(debug=True,host='0.0.0.0',threaded=False,port=int(os.environ.get('PORT', 80)))
 import os
 from subprocess import check_output
 from flask import Flask
@@ -52,7 +18,7 @@ def post():
     value2 = request.form['content']
     list_news=Generatored.generate(initial_content=value2,title=value1)
     listToStr = ' '.join(map(str,list_news))
-    return listToStr
+    return render_template('result.html',value=listToStr)
 @app.route('/api/')
 def api():
     titled=request.args.get('title')
@@ -61,12 +27,5 @@ def api():
     list_news=Generatored.generate(initial_content=contented,title=titled)
     listToStr = ' '.join(map(str,list_news))
     return listToStr
-#     except:
-#         return 'to generate article please add /api/title/content at the end of url'
-    # return title+content
-        # return check_output(f'python3 ArticleGenerator.py {keyword} -C {keyword} -p')
-    # except:
-        # return 'NO KEYWORD!!! please give the input by adding  <  ?keyword={your keyword} > at the end of url'
-
 if __name__ == "__main__":
      app.run(debug=True,host='0.0.0.0',threaded=False,port=int(os.environ.get('PORT', 80)))
